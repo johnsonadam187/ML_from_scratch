5537f4b3af94b168d8ba3af5ae976fb3719d4d8f
Adam Johnson <johnsonadam187@gmail.com>
2021-08-15 20:39:42 +1200

    added lin reg models script

---
+++ Linear_Regression_models/lin_reg_models.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def most_basic_linreg():
+    #start with random line equation
+    # get epochs (repetitions)
+    #pick learning rate
+    #pick random point
+        #if y_point > (y_line at x_point) & x_point > 
+
+    #add learning rate to slope
+    #add learning rate to intercept
+
+
+
+
+
+if __name__ == '__main__':
+    pass
